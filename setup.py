b7f73ff4d1dafb2d71efd43a4c2b9893f6a2882b
JiauZhang <1743960454@qq.com>
2023-08-08 06:57:21 +0800

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'git-picker',
+    packages = find_packages(exclude=['examples']),
+    version = '0.0.0',
+    license='MIT',
+    description = 'git picker',
+    author = 'JiauZhang',
+    author_email = 'jiauzhang@163.com',
+    url = 'https://github.com/JiauZhang/git-picker',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'GitHub',
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
