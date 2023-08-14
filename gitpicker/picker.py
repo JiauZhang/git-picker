e9813a7412dc8aaa5de0c264eb460c02789e05e3
JiauZhang <1743960454@qq.com>
2023-08-14 18:49:42 +0800

    support to download directory

--- gitpicker/picker.py
+++ gitpicker/picker.py
@@ -16,12 +16,13 @@ class Picker():
     def pick(self):
         files = self.files['file']
         for file in files:
-            url = f'{self.base_url}/{file}'
-            lines = self.download_file(url)
-            filename = f'{self.repo}/{file}'
-            self.save_file(filename, lines)
+            self.download_file(file)
+        dirs = self.files['dir']
+        for dir in dirs:
+            self.download_dir(dir)
 
-    def download_file(self, url):
+    def download_file(self, file):
+        url = f'{self.base_url}/{file}'
         print(f'downloading {url}')
         retry = 0
         while retry < self.retry:
@@ -34,11 +35,12 @@ class Picker():
                 retry += 1
                 print(f'retry[{retry}/{self.retry}] failed!')
                 time.sleep(1)
-        
+
         if retry == self.retry:
             raise RuntimeError('retry failed!')
 
-        return lines
+        filename = f'{self.repo}/{file}'
+        self.save_file(filename, lines)
 
     def save_file(self, filename, lines):
         dirname = osp.dirname(filename)
@@ -48,3 +50,36 @@ class Picker():
             for i in range(len(lines)-1):
                 f.write(lines[i] + '\n')
             f.write(lines[-1])
+
+    def download_dir(self, dir):
+        url = f'{self.base_url}/{dir}'
+        print(f'downloading {url}')
+        retry = 0
+        while retry < self.retry:
+            try:
+                r = requests.get(url)
+                js = json.loads(r.text)
+                items = js['payload']['tree']['items']
+                break
+            except:
+                retry += 1
+                print(f'retry[{retry}/{self.retry}] failed!')
+                time.sleep(1)
+
+        if retry == self.retry:
+            raise RuntimeError('retry failed!')
+
+        _dirs, _files = [], []
+        for item in items:
+            ctype = item['contentType']
+            if ctype == 'directory':
+                _dirs.append(item['path'])
+            elif ctype == 'file':
+                _files.append(item['path'])
+            else:
+                raise RuntimeError(f'unsupport contentType: {ctype}')
+
+        for file in _files:
+            self.download_file(file)
+        for dir in _dirs:
+            self.download_dir(dir)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'git-picker',
     packages = find_packages(exclude=['examples']),
-    version = '0.0.2',
+    version = '0.0.3',
     license='MIT',
     description = 'git picker',
     author = 'JiauZhang',
