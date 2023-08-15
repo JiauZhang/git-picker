a2c56aefdb754d6c7094e9e93d040742d9432bf3
JiauZhang <1743960454@qq.com>
2023-08-15 18:15:56 +0800

    support multi-threading

--- gitpicker/picker.py
+++ gitpicker/picker.py
@@ -1,25 +1,52 @@
 import os, json, requests, time
 import os.path as osp
+import threading as td
+from queue import Queue
 
 class Picker():
-    def __init__(self, user, repo, branch, files, retry=10):
+    def __init__(self, user, repo, branch, files, retry=10, threads=os.cpu_count()):
         self.user = user
         self.repo = repo
         self.branch = branch
         self.files = files
         self.retry = retry
         self.base_url = f'https://github.com/{self.user}/{self.repo}/blob/{self.branch}'
+        self.tasks = Queue()
+        self.lock = td.Lock()
+        self.threads = threads
+        self.running = False
 
         if not osp.exists(self.repo):
            os.makedirs(self.repo)
 
     def pick(self):
+        self.running = True
+        threads = [td.Thread(target=self.download_thread) for _ in range(self.threads)]
+        _ = [thread.start() for thread in threads]
+
         files = self.files['file']
+        self.lock.acquire()
         for file in files:
-            self.download_file(file)
+            self.tasks.put(file)
+        self.lock.release()
         dirs = self.files['dir']
         for dir in dirs:
             self.download_dir(dir)
+
+        self.running = False
+        _ = [thread.join() for thread in threads]
+
+    def download_thread(self):
+        while (self.running or not self.tasks.empty()):
+            if not self.tasks.empty():
+                self.lock.acquire()
+                task = None
+                if not self.tasks.empty():
+                    task = self.tasks.get()
+                self.lock.release()
+                if task: self.download_file(task)
+            else:
+                time.sleep(1)
 
     def download_file(self, file):
         url = f'{self.base_url}/{file}'
@@ -79,7 +106,9 @@ class Picker():
             else:
                 raise RuntimeError(f'unsupport contentType: {ctype}')
 
+        self.lock.acquire()
         for file in _files:
-            self.download_file(file)
+            self.tasks.put(file)
+        self.lock.release()
         for dir in _dirs:
             self.download_dir(dir)
